ac3e2eeebff9976b3729000fe50e171616b3dd1a
stephen pesce <stevepesce879@gmail.com>
2020-05-01 20:10:54 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -42,7 +42,7 @@ def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
-  puts "test: ${matrix}" 
+  matrix.each.each {|n| puts n }
   matrix[row][column] = new_value
   puts "test: ${matrix}" 
 end

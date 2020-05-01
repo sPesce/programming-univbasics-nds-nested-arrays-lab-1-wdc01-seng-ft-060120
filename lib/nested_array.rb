4fbdfba608355354146b397dca09306ce7c48a1f
stephen pesce <stevepesce879@gmail.com>
2020-05-01 20:05:39 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,41 +1,73 @@
 # Examples inspired by U. S. National Organic Standards
 
 ORGANIC_PRODUCE = [
-  "Strawberries",
-  "Potatoes",
-  "Grapes",
-  "Avocadoes",
-  "Asparagus"
+  "Strawberries",   #0 
+  "Potatoes",       #1 
+  "Grapes",         #2 
+  "Avocadoes",      #3
+  "Asparagus"       #4
 ]
 
 CONVENTIONAL_PRODUCE = [
-  "Grapefruit",
-  "Pineapple",
-  "Oranges",
-  "Watermelon",
-  "Eggplant"
+  "Grapefruit",     #0 
+  "Pineapple",      #1 
+  "Oranges",        #2 
+  "Watermelon",     #3
+  "Eggplant"        #4
 ]
 
 def assembled_matrix
-  # Build an array that contains both of the above arrays
-  # This matrix will represent a produce storage room
-  # Organic standards require that organic products be stored ABOVE conventional, not the other way around
-  # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
+
+  assembled = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
+
 end
 
 def sorted_matrix
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
+  conv_sorted = CONVENTIONAL_PRODUCE.sort 
+  org_sorted = ORGANIC_PRODUCE.sort 
+
+  sorted = [conv_sorted, org_sorted]  
+  
 end
 
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
+
+  matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
+  matrix[row][column] = new_value
+end
+
+
+#not used
+def bubble_sort(arr)
+
+  return arr if arr.length <= 1 
+  temp = ""      
+  n = 0 
+  swapped = true 
+  
+  debug = 0;
+  
+  while (swapped) do
+    swapped = false 
+    while (n < (arr.length)) do
+      if arr[n] > arr[n + 1]
+        temp = arr[n]
+        arr[n] = arr[n + 1]
+        arr[n+1] = temp 
+        swapped = true
+      end
+    end
+    debug += 1 
+    puts debug 
+  end
+  arr 
 end
